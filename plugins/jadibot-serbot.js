ee0c0ee2ea2ede3ff31cfe5ba9072992c201c0b9
the-27 <blackoficial2025@gmail.com>
2025-07-29 10:34:14 -0500

    𝐃𝐞𝐯.𝐒𝐡𝐚𝐝𝐨𝐰 💨

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -42,7 +42,7 @@ if (global.conns instanceof Array) console.log()
 else global.conns = []
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
 if (!globalThis.db.data.settings[conn.user.jid].jadibotmd) return conn.reply(m.chat,`🌳 El Comando *${command}* está desactivado temporalmente.`, m, fake)
-}
+
 let time = global.db.data.users[m.sender].Subs + 120000
 if (new Date - global.db.data.users[m.sender].Subs < 120000) return conn.reply(m.chat, `${emoji} Debes esperar ${msToTime(time - new Date())} para volver a vincular un *Sub-Bot.*`, m, fake)
 const subBots = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
