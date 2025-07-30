59cf44beb648fc302f94ea5f8c41e154008bbe90
the-27 <blackoficial2025@gmail.com>
2025-07-30 16:40:22 -0500

    𝐃𝐞𝐯.𝐒𝐡𝐚𝐝𝐨𝐰

--- plugins/buscador-playstore.js
+++ plugins/buscador-playstore.js
@@ -1,4 +1,4 @@
-/*import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 const handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
@@ -43,39 +43,4 @@ handler.command = ['playstore'];
 handler.help = ['playstore <nombre>'];
 handler.tags = ['buscador'];
 
-export default handler;*/
-
-
-import gplay from "google-play-scraper";
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return conn.reply(m.chat, `*${xsearch} Ingresa el nombre de la APP que deseas buscar en la PlayStore*`, m);
-  }
-  let res = await gplay.search({ term: text });
-  if (!res.length) {
-    return conn.reply(m.chat, "```⚠️ No se encontraron resultados, intente con otra busqueda```", m); 
-  }
-  let opt = {
-    contextInfo: {
-      externalAdReply: {
-        title: res[0].title,
-        body: res[0].summary,
-        thumbnail: (await conn.getFile(res[0].icon)).data,
-        sourceUrl: res[0].url,
-      },
-    },
-  };
-  res = res.map(
-    (v) =>
-      `*🤍 \`Resultado:\`* ${v.title}
-       *☕ \`Desarrollador:\`* ${v.developer}
-       *💸 \`Precio:\`* ${v.priceText || "Gratis"}
-       *📈 \`Puntuación:\`* ${v.scoreText || "Sin Puntuación"}
-       *⛓️ \`Link:\`* ${v.url}`
-  ).join("\n\n");
-  conn.reply(m.chat, res, m, opt); 
-};
-handler.help = ['playstoresearch *<texto>*']; 
-handler.tags = ['buscador'];
-handler.command = /^(playstoresearch|pssearch)$/i; 
 export default handler;
