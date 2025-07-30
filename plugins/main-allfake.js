ad0a66cd269be6921bad7dfdc2ccddf8a700724d
the-27 <blackoficial2025@gmail.com>
2025-07-30 10:53:07 -0500

    𝐃𝐞𝐯.𝐒𝐡𝐚𝐝𝐨𝐰 𝐂𝐨𝐫𝐞

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -113,7 +113,9 @@ global.icono = [
 'https://files.catbox.moe/qnvpyq.jpg'
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = global.fake
+
+//global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
 export default handler
 
