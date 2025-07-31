a59eb4cdcffdd83b07d55bb030e889d98fd72cab
the-27 <blackoficial2025@gmail.com>
2025-07-31 06:40:10 -0500

    𝐃𝐞𝐯.𝐒𝐡𝐚𝐝𝐨𝐰

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -53,7 +53,7 @@ const handler = async (m, { conn, text, command }) => {
       throw new Error('⚠️ No se pudo generar el enlace de descarga.');
     }
 
-    await conn.sendMessage(m.chat, {
+   /* await conn.sendMessage(m.chat, {
       audio: { url: json.download },
       mimetype: 'audio/mpeg',
       fileName: `${json.title}.mp3`,
@@ -68,7 +68,9 @@ const handler = async (m, { conn, text, command }) => {
           renderLargerThumbnail: true
         }
       }
-    }, { quoted: fkontak });
+    }, { quoted: fkontak });*/
+    
+    await conn.sendMessage(m.chat, { audio: { url: json.download }, fileName: `${json.title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: fkontak })
 
     await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
 
