2cda24004e8e800c7f507aa6edfda5d51fb7de8e
the-27 <blackoficial2025@gmail.com>
2025-08-01 06:52:27 -0500

    𝐃𝐞𝐯.𝐒𝐡𝐚𝐝𝐨𝐰

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -47,8 +47,8 @@ let time = global.db.data.users[m.sender].Subs + 120000
 if (new Date - global.db.data.users[m.sender].Subs < 120000) return conn.reply(m.chat, `${emoji} Debes esperar ${msToTime(time - new Date())} para volver a vincular un *Sub-Bot.*`, m, fake)
 const subBots = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
 const subBotsCount = subBots.length
-if (subBotsCount === 20) {
-return m.reply(`${emoji2} No se han encontrado espacios para *Sub-Bots* disponibles.`)
+if (subBotsCount === 30) {
+return m.reply(`${emoji6} No se han encontrado espacios para *Sub-Bots* disponibles.`)
 }
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let id = `${who.split`@`[0]}`
