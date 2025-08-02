a83fa74405d9a18d9f7d7dec8db6760d970c1f84
the-27 <blackoficial2025@gmail.com>
2025-08-02 06:26:39 -0500

    𝐃𝐞𝐯.𝐒𝐡𝐚𝐝𝐨𝐰

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,13 +1,20 @@
 let media = './src/catalogo.jpg'
 let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
-let str = `𝐻𝑂𝐿𝐴 𝐵𝐼𝐸𝑁𝑉𝐸𝑁𝐼𝐷𝑂 𝐸𝑆𝑇𝐸 𝐸𝑆 𝐸𝑁 𝑀𝐸𝑁𝑈 𝐷𝐸 𝐿𝐴 𝐴𝐿𝑉 𝑋𝐷`
-await conn.sendButton(m.chat, str, `𝐃𝐄𝐕.𝐒𝐇𝐀𝐃𝐎𝐖\n${wm}\n\n` + wm, media, [
-['𝗚𝗥𝗨𝗣𝗢𝗦 ~', '.grupos'],
-['𝗖𝗥𝗘𝗔𝗗𝗢𝗥 • 𝗢𝗙𝗖', '#owner'],
+let str = `╭─〔 ⚔️ 𝐒𝐔𝐊𝐔𝐍𝐀 𝐌𝐄𝐍𝐔 𝐗𝐃 🐾 〕─╮
+┃ ✦ ¡𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨, 𝐜𝐚𝐦𝐩𝐞𝐨𝐧 𝐝𝐞𝐥 𝐜𝐚𝐚𝐨𝐬!
+┃ ✦ Este es el rincón maldito del bot 🔥
+┃ ✦ Explora las funciones oscuras y letales...
+┃ ✦ Usa los botones para navegar el inframundo 👹
+╰─────────────────────────⬣`
+await conn.sendButton(m.chat, str, `☁️ 𝐃𝐄𝐕.𝐒𝐇𝐀𝐃𝐎𝐖\n${club}\n\n` + wm, media, [
+['📢 𝗚𝗥𝗨𝗣𝗢𝗦 ~', '.grupos'],
+['👤 𝗖𝗥𝗘𝗔𝗗𝗢𝗥 • 𝗢𝗙𝗖', '#owner'],
 ['☘️ 𝗠𝗘𝗡𝗨 • 𝗔𝗟𝗟', '/menu']], null, [
-['𝗚𝗜𝗧𝗛𝗨𝗕', `https://github.com/Yuji-XDev/SukunaUltra-MD`]], fkontak)}
+['🌐 𝗚𝗜𝗧𝗛𝗨𝗕', `https://github.com/Yuji-XDev/SukunaUltra-MD`]], fkontak)}
 
-handler.command = ['alv']
-handler.exp = 80
+handler.help = ['donar']
+handler.tags = ['info']
+handler.command = ['donar', 'alv']
+handler.exp = 200
 export default handler
