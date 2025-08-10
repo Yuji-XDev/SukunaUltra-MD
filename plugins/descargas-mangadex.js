a81b7d6dc2cf681bc09b805d07b6c7e18251e003
the-27 <blackoficial2025@gmail.com>
2025-08-10 12:18:22 -0500

    Update

--- plugins/descargas-mangadex.js
+++ plugins/descargas-mangadex.js
@@ -100,7 +100,7 @@ let handler = async (m, { conn, args }) => {
 
 handler.help = ["mangad <nombre del manga> <número del capítulo>"];
 handler.tags = ['descargas'];
-handler.command = /^(mangad)$/i;
-handler.estrellas = 8;
+handler.command = ['mangad']i;
+handler.coin = 8;
 
 export default handler;
